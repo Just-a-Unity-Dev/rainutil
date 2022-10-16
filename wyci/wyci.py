c1ec5cc04a948446af6f56684a7cb19b412b42a9
Just-a-Unity-Dev <just-a-unity-dev@users.noreply.github.com>
2022-10-16 21:02:28 +0800

    proper wyci

--- wyci/wyci.py
+++ wyci/wyci.py
@@ -15,30 +15,30 @@ class Wyci(commands.Cog):
         }
         self.config.register_guild(**default_guild)
 
-    async def remove_punctuation(self, x: str):
-        return x.translate(str.maketrans('', '', string.punctuation))
+    @commands.group()
+    async def wyci(self, ctx):
+        """When You Code It!!! (x67)"""
 
     @commands.Cog.listener()
-    async def on_message(self, message: discord.Message):
-        content: str = message.content
-        count = 0
-        for match in re.finditer(r"when$", self.remove_punctuation(content.lower())):
-            async with self.config.guild(message.channel.guild).counter() as count:
-                count += 1
-                if count == 1:
-                    return await message.channel.send("When You Code It!!!")
-                else:
-                    return await message.channel.send(f"When You Code It!!! (x{count})")
+    async def on_message(self, message):
+        for match in re.finditer(r"\S\s+(?:when|whence)[\s*?.!)]*$", message.content):
+            counter = self.config.guild(message.channel.guild).counter
+            count = await counter()
+            await counter.set(count + 1)
+            if (count + 1) <= 1:
+                return await message.channel.send("When You Code It!!!")
+            else:
+                return await message.channel.send(f"When You Code It!!! (x{count + 1})")
         
-    @commands.group()
+    @wyci.group()
     @checks.admin_or_permissions(manage_guild=True)
-    async def config(self):
+    async def config(self,  ctx):
         """Configuration for WYCI"""
         pass
 
-    @config.command(aliases=["resetcounter"])
-    async def config_reset_counter(self, ctx: discord.Context):
+    @config.command(name="resetcounter")
+    async def config_reset_counter(self, ctx):
         """Resets the WYCI counter"""
-        async with self.config.guild(message.channel.guild).counter() as count:
-            count = 0
+        count = self.config.guild(ctx.guild).counter
+        await count.set(0)
         return await ctx.reply("Reset the WYCI counter.")
